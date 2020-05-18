f19d0c0eedd600491d8cbfa17e72b23a36960b19
Eric <Aerelioyanadel@gmail.com>
2020-05-18 13:38:53 -0400

    add borwser router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react"
+import { BrowserRouter as Router, Router } from "react-router-dom"
 import "./App.css"
 
 function App() {
-  return <div className="container">hello</div>
+  return (
+    <div className="container">
+      <Router>
+        <h1> hello </h1>
+      </Router>
+    </div>
+  )
 }
 
 export default App
